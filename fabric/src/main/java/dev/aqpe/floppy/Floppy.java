6c6214c2a853dc852010748fd030102a8b1cc875
Aperre <loleur12345@gmail.com>
2026-02-08 20:42:34 +0100

    idk anymore man

---
+++ fabric/src/main/java/dev/aqpe/floppy/Floppy.java
@@ -0,0 +1,15 @@
+package dev.aqpe.floppy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Floppy implements ModInitializer {
+	public static final String MOD_ID = "floppy";
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
