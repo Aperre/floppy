e1c5095a58f9f2a4443218393152d8d8220ccd35
Aperre <loleur12345@gmail.com>
2026-02-08 13:24:06 +0100

    Setup before

--- common/src/main/java/dev/aqpe/floppy/Floppy.java
+++ common/src/main/java/dev/aqpe/floppy/Floppy.java
@@ -12,17 +12,8 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Floppy.MODID)
 public class Floppy {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "floppy";
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public Floppy(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-    }
 
     private void commonSetup(FMLCommonSetupEvent event) {
         // Some common setup code
